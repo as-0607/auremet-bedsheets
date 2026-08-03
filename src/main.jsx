b97cf1935b526536e9f4143b36e7bc3b810d93ef
Abdelkader Ayman <abdelkader.ayman6@gmail.com>
2026-08-03 18:19:53 +0300

    Set basename for BrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import ScrollToTop from "./components/ScrollToTop.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/bedsheets-website">
       <ScrollToTop />
       <App />
     </BrowserRouter>
