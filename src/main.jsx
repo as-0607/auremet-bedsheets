d0478aeeef5da649cd14cf9ee9d8dae7d8b20fc5
as-0607 <abdelkaderemo@gmail.com>
2026-08-08 21:10:44 +0300

    add basename

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./i18n";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/auremet-bedsheets">
       <ScrollToTop />
       <App />
     </BrowserRouter>
